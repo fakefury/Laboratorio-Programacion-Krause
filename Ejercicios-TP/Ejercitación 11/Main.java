7ea455143df1d7f9b753632b30ad86f0375ed925
fakefury <torreskimalex@gmail.com>
2026-08-27 21:16:42 -0300

    Ejercitación-11

---
+++ Ejercicios-TP/Ejercitación 11/Main.java
@@ -0,0 +1,12 @@
+package testbd;
+
+public class Main {
+    public static void main(String[] args) {
+        Crud crud = new Crud();
+
+        crud.insertarCincoDatos();
+        crud.borrarIdCuatro();
+        crud.actualizarIdDos("Fernández", "Calzado");
+        crud.listarTodo();
+    }
+}
